d25d31ba02768665a5ecb531ff4cad34d55e1250
mikesexton <mkesexton@gmail.com>
2018-12-13 11:56:46 -0700

    Update a using statement

---
+++ Soundex.h
@@ -0,0 +1,24 @@
+//
+// Created by mikesexton on 12/13/2018.
+//
+
+#ifndef FIRSTEXAMPLE_SOUNDEX_H
+#define FIRSTEXAMPLE_SOUNDEX_H
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Soundex
+{
+private:
+    string zeroPad(const string &word) const;
+    string head(const string &word) const;
+    string encodeDigits(const string &word) const;
+ public:
+    string encode(const string &word) const;
+};
+
+
+#endif //FIRSTEXAMPLE_SOUNDEX_H
--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,41 +1,49 @@
-#include <iostream>
-#include <string>
-using namespace std;
 
-class Soundex{
-public:
-    string encode(const string &word) const
-    {
-        return word;
-    }
-};
-
+#include "Soundex.h"
 #include "gmock/gmock.h"
 using ::testing::Eq;
 
+class SoundexEncoding: public testing::Test{
+public:
+    Soundex soundex;
+};
 /**
- * Retain the first letter of the name and drop all other occurrences of a, e, i, o, u, y, h, w.
-Replace consonants with digits as follows (after the first letter):
-b, f, p, v → 1
-c, g, j, k, q, s, x, z → 2
-d, t → 3
-l → 4
-m, n → 5
-r → 6
-If two or more letters with the same number are adjacent in the original name (before step 1),
+1 - Retain the first letter of the name and drop all other occurrences of a, e, i, o, u, y, h, w.
+2 - Replace consonants with digits as follows (after the first letter):
+    b, f, p, v → 1
+    c, g, j, k, q, s, x, z → 2
+    d, t → 3
+    l → 4
+    m, n → 5
+    r → 6
+3 - If two or more letters with the same number are adjacent in the original name (before step 1),
  only retain the first letter; also two letters with the same number separated by 'h' or 'w' are
  coded as a single number, whereas such letters separated by a vowel are coded twice.
  This rule also applies to the first letter.
-If you have too few letters in your word that you can't assign three numbers, append with zeros
+4 - If you have too few letters in your word that you can't assign three numbers, append with zeros
  until there are three numbers. If you have more than 3 letters, just retain the first 3 numbers.
  */
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
 {
     //Arrange
-   Soundex soundex;
-   // Act
-   auto encode = soundex.encode("A");
+    // Act
    // Assert
-   ASSERT_THAT(encode, testing::Eq("A"));
+   ASSERT_THAT(soundex.encode("A"), testing::Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, PadsWithZeroToEnsureThreeDigits)
+{
+    //Arrange
+    // Act
+    // Assert
+    ASSERT_THAT(soundex.encode("I"), testing::Eq("I000"));
+}
+
+TEST_F(SoundexEncoding, ReplaceConsonantsWithAppropriateLetters)
+{
+    //Arrange
+    // Act
+    // Assert
+    ASSERT_THAT(soundex.encode("Ab"), testing::Eq("A100"));
 }
 
